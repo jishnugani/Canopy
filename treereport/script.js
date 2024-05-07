ad50c4721a5e6f778eb45518abca359dcff67da7
jishnugani <155496973+jishnugani@users.noreply.github.com>
2024-05-07 15:24:47 -0700

    Added Tree Mapping

---
+++ treereport/script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('tree-report');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    alert("Thank you! Tree report submitted for review.");
+    
+});
